4faff8ba6e1c95eb0284264058b0ea82771e6987
Leonardo Costa <leosilvacosta6@gmail.com>
2026-03-30 18:05:31 -0300

    inline operator<< for SQLData

--- include/sql_cpp_wrapper/DBAbstractionLayer.hpp
+++ include/sql_cpp_wrapper/DBAbstractionLayer.hpp
@@ -48,7 +48,7 @@ enum class SQLDataType
     Text,        // std::string
 };
 
-std::ostream& operator<<(std::ostream& os, const SQLData& v)
+inline std::ostream& operator<<(std::ostream& os, const SQLData& v)
 {
     std::visit(
         [&os](auto&& arg) {
